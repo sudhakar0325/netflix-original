20b711020acbf2c2cd0cccf969d7e2aa3291efd1
sudhakar0325 <129932680+sudhakar0325@users.noreply.github.com>
2023-04-17 22:05:44 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,92 @@
+let con1=document.getElementById("sec1");
+
+function change(){
+    
+    let di=document.getElementById("rrr");
+    di.classList.add("detsec");
+    di.classList.toggle("none")
+    let p=document.getElementById("para");
+    p.textContent="Netflix is a streaming service that offers a wide variety of award-winning TV shows, movies, anime, documentaries and more – on thousands of internet-connected devices.";
+    p.style.color="red";
+
+}
+con1.addEventListener("click",change);
+let con2=document.getElementById("sec2");
+
+function change2(){
+    let di=document.getElementById("rrr");
+    di.classList.add("detsec");
+    di.classList.toggle("none")
+    let p=document.getElementById("para");
+    p.textContent="Watch Netflix on your smartphone, tablet, Smart TV, laptop, or streaming device, all for one fixed monthly fee. Plans range from ₹ 149 to ₹ 649 a month. No extra costs, no contracts.";
+    p.style.color="red";
+
+}
+con2.addEventListener("click",change2);
+let con3=document.getElementById("sec3");
+
+function change3(){
+   
+    let di=document.getElementById("rrr");
+    di.classList.add("detsec");
+    di.classList.toggle("none")
+    let p=document.getElementById("para");
+    p.textContent="Watch anywhere, anytime. Sign in with your Netflix account to watch instantly on the web at netflix.com from your personal computer or on any internet-connected device that offers the Netflix app, including smart TVs, smartphones, tablets, streaming media players and game consoles.";
+    p.style.color="red";
+
+}
+con3.addEventListener("click",change3);
+
+let con5=document.getElementById("sec5");
+
+function change5(){
+   
+    let di=document.getElementById("rrr");
+    di.classList.add("detsec");
+    di.classList.toggle("none")
+    let p=document.getElementById("para");
+    p.textContent="Netflix has an extensive library of feature films, documentaries, TV shows, anime, award-winning Netflix originals, and more. Watch as much as you want, anytime you want.";
+    p.style.color="red";
+
+}
+con5.addEventListener("click",change5);
+let con4=document.getElementById("sec4");
+
+function change4(){
+   
+    let di=document.getElementById("rrr");
+    di.classList.add("detsec");
+    di.classList.toggle("none")
+    let p=document.getElementById("para");
+    p.textContent="Netflix is flexible. There are no annoying contracts and no commitments. You can easily cancel your account online in two clicks. There are no cancellation fees – start or stop your account anytime.";
+    p.style.color="red";
+
+}
+con4.addEventListener("click",change4);
+let con6=document.getElementById("sec6");
+
+function change6(){
+   
+    let di=document.getElementById("rrr");
+    di.classList.add("detsec");
+    di.classList.toggle("none")
+    let p=document.getElementById("para");
+    p.textContent="The Netflix Kids experience is included in your membership to give parents control while kids enjoy family-friendly TV shows and films in their own space.";
+    p.style.color="red";
+    
+
+}
+con6.addEventListener("click",change6);
+//how to redirect to sign in page when i click sign in button?
+
+
+
+
+
+
+
+
+
+
+
+
